480e4cf3b78f4b58e17f2548a4d00a4d794a6291
Romendra Uttam <7388romendra@gmail.com>
2024-01-22 21:30:54 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from 'mongoose'
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js'
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 
 app.listen(3000, () => {
@@ -24,3 +26,4 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
