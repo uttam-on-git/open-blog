02c6a4b5caa27930bb26a91bfd47820389e2d8df
Romendra Uttam <7388romendra@gmail.com>
2024-01-24 00:13:13 +0530

    complete signin page funtionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,110 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button, Alert, Label, TextInput, Spinner} from 'flowbite-react';
 
-export default function Signin() {
+export default function SignIn() {
+
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try{
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/')
+      }
+    } catch (error) {
+        setErrorMessage(error.message);
+        setLoading(false)
+    }
+  }
+
   return (
-    <div>Signin</div>
-  )
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row 
+      md:items-center gap-2">
+        {/* left */}
+        <div className="flex-1">
+          <Link
+            to="/"
+            className="self-center whitespace-nowrap font-semibold
+         dark:text-white text-4xl"
+          >
+            <span
+              className="px-2 py-1 bg-gradient-to-r from-indigo-600
+         rounded-lg text-black"
+            >
+              Open blog
+            </span>
+          </Link>
+
+          <p className="text-sm mt-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla
+            aspernatur placeat quibusdam earum? Maiores velit eveniet,
+            temporibus numquam quasi facilis reprehenderit quo? Consequatur
+            alias, est libero ducimus blanditiis ab id?
+          </p>
+        </div>
+        {/*right side*/}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+
+            <div>
+            <Label value="Your email"/>
+            <TextInput type='email' placeholder='mail@company.com' id='email' onChange={handleChange}/>
+            </div>
+
+            <div>
+            <Label value="Your password"/>
+            <TextInput type='password' placeholder='********' id='password' onChange={handleChange}/>
+            </div>
+            <Button gradientDuoTone='purpleToPink' type="submit" disabled={loading}>
+              {loading ? (
+                <>
+                <Spinner size='sm'/>
+                <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                'Sign Ip'
+              )}
+            </Button>
+
+          </form>
+          <div className="flex gap-4 text-sm mt-5">
+            <span>Don't have an account?</span>
+              <Link to='/sign-up' className="text-blue-500">
+                Sign Up
+              </Link>
+          </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
+        </div>
+      </div>
+    </div>
+  );
 }
