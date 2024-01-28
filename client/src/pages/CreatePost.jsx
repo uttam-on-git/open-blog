253804ec64cfabe742e04f9690ddca4a8058ae3b
Romendra Uttam <120070407+uttam-on-git@users.noreply.github.com>
2024-01-28 12:55:57 +0530

    complete upload image functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,30 +1,121 @@
-import { TextInput, Select, FileInput, Button } from 'flowbite-react'
-import React from 'react'
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import { TextInput, Select, FileInput, Button, Alert } from "flowbite-react";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { getStorage } from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function CreatePost() {
+  const [file, setFile] = useState(null);
+  const [imageUploadProgress, setImageUploadProgess] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+
+  const handleUploadImage = async () => {
+    try {
+      if (!file) {
+        setImageUploadError("Please select an image");
+        return;
+      }
+      setImageUploadError(null);
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + "_" + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageUploadProgess(progress.toFixed(0));
+        },
+        (error) => {
+          setImageUploadError("Image upload failed");
+          setImageUploadProgess(null);
+        },
+        () => {
+          getDownload(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUploadError(null);
+            setImageUploadProgess(null);
+            setFormData({ ...formData, image: downloadURL });
+          });
+        }
+      );
+    } catch (error) {
+      setImageUploadError("Image upload failed");
+      setImageUploadProgess(null);
+    }
+  };
   return (
-    <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-        <h1 className='text-center text-3xl my-7 font-semibold'>Create a post </h1>
-        <form className='flex flex-col gap-4'>
-            <div className='flex flex-col gap-4 sm:flex-row justify-between'>
-                <TextInput type='text' placeholder='Title' required id='title' className='flex-1'/>
-                <Select>
-                    <option value='uncategorized'>Select a category</option>
-                    <option value='url-hortner'>Url Shortner</option>
-                    <option value='reactjs'>React.js</option>
-                    <option value='system-design'>System design</option>
-                </Select>
-            </div>
-            <div className='flex gap-4 items-center justify-between border-4 *
-            border-teal-500 border-dotted p-3'>
-                <FileInput type='file' accept='image/*'/>
-                <Button type='button' size='sm' outline>Upload Image</Button>
-            </div>
-            <ReactQuill theme="snow" placeholder='Write Something' className='h-72 mb-12' required/>
-            <Button type='submit' outline >Publish</Button>
-        </form>
+    <div className="p-3 max-w-3xl mx-auto min-h-screen">
+      <h1 className="text-center text-3xl my-7 font-semibold">
+        Create a post{" "}
+      </h1>
+      <form className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 sm:flex-row justify-between">
+          <TextInput
+            type="text"
+            placeholder="Title"
+            required
+            id="title"
+            className="flex-1"
+          />
+          <Select>
+            <option value="uncategorized">Select a category</option>
+            <option value="url-hortner">Url Shortner</option>
+            <option value="reactjs">React.js</option>
+            <option value="system-design">System design</option>
+          </Select>
+        </div>
+        <div
+          className="flex gap-4 items-center justify-between border-4 *
+            border-teal-500 border-dotted p-3"
+        >
+          <FileInput
+            type="file"
+            accept="image/*"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <Button
+            type="button"
+            size="sm"
+            outline
+            onClick={handleUploadImage}
+            disabled={imageUploadProgress}
+          >
+            {imageUploadProgress ? (
+              <div className="w-16 h-16">
+                <CircularProgressbar
+                  value={imageUploadProgress}
+                  text={`${imageUploadProgress || 0}%`}
+                />
+              </div>
+            ) : (
+              "Upload Image"
+            )}
+          </Button>
+        </div>
+        {imageUploadError && <Alert color="failure">{imageUploadError}</Alert>}
+        {formData.image && (
+          <img
+            src={formData.image}
+            alt="upload"
+            className="w-full h-72 object-cover"
+          />
+        )}
+
+        <ReactQuill
+          theme="snow"
+          placeholder="Write Something"
+          className="h-72 mb-12"
+          required
+        />
+        <Button type="submit" outline>
+          Publish
+        </Button>
+      </form>
     </div>
-  )
+  );
 }
